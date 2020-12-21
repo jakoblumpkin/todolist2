7296eadba4eb012a1e1cdf5258ce1b9cf41f074a
jakob lumpkin <lumpkinjakobr@gmail.com>
2020-12-21 13:43:42 -0600

    del

--- list/list/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),
-    path('delete/<int:pk>/', views.delete),
-    path('deleteall/', views.delete_all),
-    path('register/', views.register),
-]
